d6250fda9964f7c1f815d272baebe8552c5e3a72
JRobsonJr <jrobsonjr16@gmail.com>
2018-09-23 17:56:13 -0300

    Add missing flow annotations and configurations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+/* @flow */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/app/App';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root'),
-);
+const root = document.getElementById('root');
+
+if (root) {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>,
+        root,
+    );
+}
