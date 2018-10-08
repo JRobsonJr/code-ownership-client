5a3ffaa0225c3dee4604d4160d52fa770b762281
JRobsonJr <jrobsonjr16@gmail.com>
2018-10-08 18:09:59 -0300

    Implement getStudents and getArtifacts with mock data

---
+++ src/api/mock-api.js
@@ -0,0 +1,28 @@
+/* @flow */
+
+import { Student, Artifact } from './models';
+
+export const getStudents = () => {
+    const david = new Student('David', 'https://goo.gl/9b23Uw');
+    david.addAlias('davidedup');
+    david.addAlias('DAVID EDUARDO PEREIRA 116110000');
+
+    const robson = new Student('Robson', 'https://goo.gl/aEPtCY');
+    robson.addAlias('JRobsonJr');
+    robson.addAlias('SoftCircuits');
+
+    const mariana = new Student('Mariana', 'https://goo.gl/JSHM33');
+    mariana.addAlias('mariana-mendes');
+    mariana.addAlias('Mariana e Silva');
+
+    const matheus = new Student('Matheus', 'https://goo.gl/2vQ12L');
+    matheus.addAlias('matheusgr');
+
+    return [david, robson, mariana, matheus];
+};
+
+export const getArtifacts = () => [
+    new Artifact('src/main/Main.java'),
+    new Artifact('src/genre/Genre.java'),
+    new Artifact('src/genre/Pop.java'),
+];
