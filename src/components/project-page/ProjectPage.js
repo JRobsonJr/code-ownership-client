1ae86e267bcf1391087b38f78f716cb7e1426827
David <david.pereira@ccc.ufcg.edu.br>
2018-10-13 16:45:41 -0300

    Calling new coponent at the ProjectPageComponent

--- src/components/project-page/ProjectPage.js
+++ src/components/project-page/ProjectPage.js
@@ -5,12 +5,16 @@ import React, { Component } from 'react';
 import { Grid } from '@material-ui/core';
 
 import StudentList from './StudentList';
+import ArtifactList from './ArtifactList';
 
 import { getStudents } from '../../api/mock-api';
+import { getArtifacts } from '../../api/mock-api';
 import { Student } from '../../api/models';
+import { Artifact } from '../../api/models';
 
 type State = {
     students: Array<Student>,
+    artifacts: Array<Artifact>;
 };
 
 class ProjectPage extends Component<{}, State> {
@@ -19,18 +23,20 @@ class ProjectPage extends Component<{}, State> {
 
         this.state = {
             students: getStudents(),
+            artifacts: getArtifacts(),
         };
     }
 
     render() {
-        const { students } = this.state;
+        const { students, artifacts } = this.state;
 
         return (
             <Grid
               container
               justify="center"
             >
-                <StudentList students={students} />
+                    <StudentList students={students} />
+                    <ArtifactList artifacts={artifacts} />   
             </Grid>
         );
     }
