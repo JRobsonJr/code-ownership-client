9afc9de392aedd154c56845f1076d94463fd306d
David <david.pereira@ccc.ufcg.edu.br>
2018-10-13 16:44:54 -0300

    Creating ArtifactList component

---
+++ src/components/project-page/ArtifactList.js
@@ -0,0 +1,52 @@
+/* @flow */
+
+import React from 'react';
+
+import{
+    Grid,
+    Typography,
+    List,
+    Paper,
+    Divider,
+    withStyles,
+} from '@material-ui/core';
+
+import ArtifactListItem from './ArtifactListItem';
+
+import { Artifact } from '../../api/models';
+
+type Props = {
+    artifacts: Array<Artifact>,
+};
+
+const styles = theme => ({
+    title: {
+        marginTop: theme.spacing.unit,
+        padding: theme.spacing.unit,
+    },
+});
+
+const ArtifactList = ({ artifacts }: Props) => {
+    const artifactListItems = artifacts
+        .map(artifact => <ArtifactListItem artifact={artifact} />);
+
+    return (
+        <Grid item xs={6}>
+            <Paper elevation={2}>
+                <Typography
+                  variant="title"
+                  align="center"
+                >
+                    ARTIFACT
+                </Typography>
+                <Divider />
+                <List>
+                    {artifactListItems}
+                </List>
+            </Paper>
+        </Grid>
+    );
+};
+
+
+export default  withStyles(styles)(ArtifactList);
