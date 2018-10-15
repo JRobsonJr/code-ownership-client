55de613fbb13501b02e02d7924bfce7515303450
JRobsonJr <jrobsonjr16@gmail.com>
2018-10-15 18:31:15 -0300

    Update project page with new and updated implementations

--- src/components/project-page/ProjectPage.js
+++ src/components/project-page/ProjectPage.js
@@ -1,43 +1,62 @@
 /* @flow */
 
 import React, { Component } from 'react';
+import { Grid, withStyles } from '@material-ui/core';
 
-import { Grid } from '@material-ui/core';
-
+import ProjectNameGrid from './ProjectNameGrid';
 import StudentList from './StudentList';
 import ArtifactList from './ArtifactList';
 
-import { getStudents, getArtifacts } from '../../api/mock-api';
+import { getProjectName, getStudents, getArtifacts } from '../../api/mock-api';
 import { Student, Artifact } from '../../api/models';
 
+type Props = {
+    classes: any,
+}
+
 type State = {
+    projectName: string,
     students: Array<Student>,
-    artifacts: Array<Artifact>;
+    artifacts: Array<Artifact>,
 };
 
-class ProjectPage extends Component<{}, State> {
+const styles = theme => ({
+    root: {
+        padding: theme.spacing.unit,
+    },
+});
+
+class ProjectPage extends Component<Props, State> {
     constructor() {
         super();
 
         this.state = {
+            projectName: getProjectName(),
             students: getStudents(),
             artifacts: getArtifacts(),
         };
     }
 
     render() {
-        const { students, artifacts } = this.state;
+        const { classes } = this.props;
+        const { projectName, students, artifacts } = this.state;
 
         return (
-            <Grid
-              container
-              justify="center"
-            >
-                <StudentList students={students} />
-                <ArtifactList artifacts={artifacts} />
-            </Grid>
+            <div className={classes.root}>
+                <Grid
+                  container
+                  spacing={16}
+                  alignItems="center"
+                  direction="row"
+                  justify="center"
+                >
+                    <ProjectNameGrid projectName={projectName} />
+                    <StudentList students={students} />
+                    <ArtifactList artifacts={artifacts} />
+                </Grid>
+            </div>
         );
     }
 }
 
-export default ProjectPage;
+export default withStyles(styles)(ProjectPage);
