e0c19fc724fdc1368c3f0b0937a0778f3fc0d4b6
JRobsonJr <jrobsonjr16@gmail.com>
2018-12-06 22:06:28 -0300

    Add GitHub handle to student model

--- src/api/mock-api.js
+++ src/api/mock-api.js
@@ -5,19 +5,21 @@ import { Student, Artifact, Contribution } from './models';
 export const getProjectName = () => 'homemade-dynamite';
 
 export const getStudents = () => {
-    const david = new Student('David', 'https://goo.gl/9b23Uw');
+    const david = new Student('David', 'https://goo.gl/9b23Uw', 'davidedup');
     david.addAlias('davidedup');
     david.addAlias('DAVID EDUARDO PEREIRA 116110000');
 
-    const robson = new Student('Robson', 'https://goo.gl/aEPtCY');
+    const robson = new Student('Robson', 'https://goo.gl/aEPtCY', 'jrobsonjr');
     robson.addAlias('JRobsonJr');
     robson.addAlias('SoftCircuits');
 
-    const mariana = new Student('Mariana', 'https://goo.gl/JSHM33');
+    const mariana =
+        new Student('Mariana', 'https://goo.gl/JSHM33', 'mariana-mendes');
     mariana.addAlias('mariana-mendes');
     mariana.addAlias('Mariana e Silva');
 
-    const matheus = new Student('Matheus', 'https://goo.gl/2vQ12L');
+    const matheus =
+        new Student('Matheus', 'https://goo.gl/2vQ12L', 'matheusgr');
     matheus.addAlias('matheusgr');
 
     return [david, robson, mariana, matheus];
